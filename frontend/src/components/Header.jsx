4ef4ff69b3cf3940d73eb749fbb328f0a170ed0c
ASHISHPUNDIR12 <pundirashish154@gmail.com>
2024-12-25 22:11:27 +0530

    fixed all bugs

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Header = ({ username }) => {
+  // localStorage.clear()
   return (
     <header className="bg-blue-500 text-white p-4">
       <h1 className="text-xl font-bold">Welcome {username}</h1>
